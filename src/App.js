32916d5467401cefb8b252ad1e9b7bb5514a1993
Gabo <gab_org@hotmail.com>
2020-03-11 17:06:11 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de seguro"></Header>
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
+  );
+}
+
+export default App;
